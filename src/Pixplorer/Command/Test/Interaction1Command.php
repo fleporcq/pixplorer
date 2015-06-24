2e819bb0fae37ba060f3ebdf9c11cb58e83ef03d
François LEPORCQ <fleporcq@gmail.com>
2015-06-24 10:24:18 +0200

    feat(test:interaction1): Mise en place

--- src/Pixplorer/Command/Test/Interaction1Command.php
+++ src/Pixplorer/Command/Test/Interaction1Command.php
@@ -5,32 +5,29 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class Test1Command extends Command
+class Interaction1Command extends Command
 {
 
     const QUIT = ':q';
 
     protected function configure()
     {
-        $this->setName('test:test1');
+        $this->setName('test:interaction1');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        define('KEYBOARD', fopen('/dev/input/event3', 'r'));
 
-        stream_set_blocking (KEYBOARD ,0);
+        stream_set_blocking(STDIN, 0);
         while (true) {
-            $line = trim(fgets(KEYBOARD));
-          $output->writeln($line.'toto');
-           /* $line = trim(fgets(STDIN));
+            $line = trim(fgets(STDIN));
             if ($line == self::QUIT) {
                 break;
             }
-            if($line) {
-                $output->writeln($line);
+            if (!empty($line)) {
+                $output->writeln('> ' . $line);
             }
-            $output->write(".");*/
+            $output->write('.');
             sleep(1);
         }
 
